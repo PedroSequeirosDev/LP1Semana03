b803b071644e346e65f644f0464a9d18729400d0
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-04 23:51:56 +0000

    added ChangeString file

---
+++ ChangeString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("String: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Caráter: ");
+            string g = Console.ReadLine();
+
+    
+            string new_string = a.Replace(g,"x");
+
+            Console.Write(new_string);
+                
+               
+
+            
+
+
+
+        
+        }
+    }
+}
