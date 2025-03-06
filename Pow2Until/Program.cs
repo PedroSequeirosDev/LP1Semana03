bce4aee018f6a32a5ea9a7ed531fc294bb075e0b
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-06 21:52:13 +0000

    added Pow2Until file

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pow2Until
+{
+    public class Program
+    {
+        private static void PowerOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        private static void Main(string[] args)
+        {
+            PowerOf2Until5();
+            PowerOf2Until5();
+
+        }
+    }
+}
